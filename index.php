f1202217714b89390fe7cabf2695c985c6cf5edc
Gabriel Luiz dos Santos Corbetta <48722454+GabrielLuizDosSantos@users.noreply.github.com>
2019-04-09 11:53:40 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,46 +1,27 @@
 <html>
-  <head>
-    <title>
-    Projeto App xxx
-    </title>
-    <style type="text/css">
-    body{
-    margin:0;
-    padding:0px;
-    background-color: #DCDCDC;
-    }
-    .topo{
-    width: 100%;
-    height: 100px; 
-    }
-    .menu{
-    width: 100%;
-   background-color: #DCDCDC;
-    }
-    .conteudo{
-    width:70%;
-    margin: 0 auto;
-    background-color: white;
-    }
-    .rodape{
-    width: 100%; height:150px;
-    background-color: #696969
-    }
-    </style>
-  </head>
-       <body>
-        <div class="topo">
-        colocar uma imagem do seu projeto
-        </div> 
-        <div class="menu">
-        colocar o menu provisorio
-        </div> 
-        <div class="conteudo">
-        conteudo principal
-        </div>
-        <div class="rodape">
-        Desenvolvido por XXX <br>
-        <a href="mailto:hjdsjfjhsdfjh@gmail.com" >Contato</a>
-        </div>
-       </body>
+	<head>
+	<title>
+		Projeto App Minha Casa minha Vida
+	</title>
+	<link type="text/css" href="estilo.css" rel="stylesheet">
+	</head>
+<body>
+	<div class="topo">
+		Colocar uma imagem do seu projeto	
+	</div>
+	<div class="menu">
+	   <ul>
+	      <li><a href="index.php">home</a></li>
+	      <li><a href="perfil.php">perfil</a></li>
+	      <li><a href="contato.php">contato</a></li>
+	   </ul>	
+	</div>
+	<div class="conteudo">
+		Conteudo principal	
+	</div>
+	<div class="rodape">
+		Desenvolvido por Gabriel luiz<br>
+		<a href="mailto:cxxxxxxxx@gmai.com">Contato</a>
+	</div>
+</body>
 </html>
